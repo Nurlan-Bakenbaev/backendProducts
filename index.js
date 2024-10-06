beceb57e8097ddf1f3f6d28d1409cdb1758e75a5
Nurlan <iamsamurai1991@gmail.com>
2024-10-06 20:58:09 +0200

    new file:   controllers/userControllers.js 	modified:   index.js 	new file:   middlewares/validator.js     new file:   models/usersModel.js 	modified:   package-lock.json
    	modified:   package.json

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { connectionDB } from "./config/db.js";
 import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
+app.use(cookieParser());
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", productRouter);
-app.use("/api", productRouter);
+app.use(cors());
+app.use("/user", userRouter);
 app.use("/api", productRouter);
 
 app.patch("/product/:id");
